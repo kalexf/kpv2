4c7f2d462b46e632d42719225f56495f0e70301f
kfry <kfry000@gmail.com>
2022-07-09 20:54:50 +0100

    update

--- runproj2/settings.py
+++ runproj2/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u!swxji^_-i*r!omej%rhg*4%x9nqr^8i&v-$6ori)(@=05@f@'
-
+SECRET_KEY = 'xxxxx' # hidden
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
